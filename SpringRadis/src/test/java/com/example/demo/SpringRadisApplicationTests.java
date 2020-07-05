5ecc5cd1610e13b4a0199dfd07c1c0b1fe0dc37b
zombiee <zombiee@zombiee-ThinkPad-X1-Carbon-3rd>
2020-07-05 15:53:22 +0530

    tset

--- SpringRadis/src/test/java/com/example/demo/SpringRadisApplicationTests.java
+++ SpringRadis/src/test/java/com/example/demo/SpringRadisApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringRadisApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	/*
+	 * @Test void contextLoads() { }
+	 */
 
 }
